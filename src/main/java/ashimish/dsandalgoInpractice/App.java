fbe2e66a0dcbe881519494ebf284153bcd972fa1
ashishkumarmishra <ashish.103022@gmail.com>
2018-03-10 09:41:46 +0530

    Creating Empty Project for DS and ALGO Practice

---
+++ src/main/java/ashimish/dsandalgoInpractice/App.java
@@ -0,0 +1,13 @@
+package ashimish.dsandalgoInpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
