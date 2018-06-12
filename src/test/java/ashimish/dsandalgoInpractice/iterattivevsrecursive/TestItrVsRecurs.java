e7161662daf344b70f035f9eb2d971dce2413aaf
AshishKrMishra <er.ashish.kr.mishra@gmail.com>
2018-06-12 23:16:44 +0530

    Adding Test Case Sample OutPut

--- src/test/java/ashimish/dsandalgoInpractice/iterattivevsrecursive/TestItrVsRecurs.java
+++ src/test/java/ashimish/dsandalgoInpractice/iterattivevsrecursive/TestItrVsRecurs.java
@@ -65,6 +65,60 @@ public class TestItrVsRecurs
 	 * Conclusion :- We can predict in this case which one is taking more time 
 	 * But one thing is clear that recursive way is not working for higher n .
 	 * After some point it start failing for each every given n 
+	 * 
+	 * 
+	 * Integer Value	Itr Way(in ms)	Recursv Way(in ms)
+			{39283} 		{0}		{0}
+			{39284} 		{0}		{0}
+			{39285} 		{0}		{0}
+			{39286} 		{0}		{0}
+			{39287} 		{0}		{0}
+			{39288} 		{0}		{0}
+			{39289} 		{0}		{0}
+			{39290} 		{0}		{0}
+			{39291} 		{1}		{0}
+			{39292} 		{0}		{0}
+			{39293} 		{0}		{1}
+			{39294} 		{0}		{0}
+			{39295} 		{0}		{0}
+			{39296} 		{0}		{0}
+			{39297} 		{0}		{0}
+			{39313} 		{0}		{0}
+			{39314} 		{0}		{0}
+			{39315} 		{0}		{0}
+			{39316} 		{0}		{0}
+			{39317} 		{0}		{0}
+			{39318} 		{0}		{0}
+			{39319} 		{0}		{0}
+			{39320} 		{0}		{0}
+			{39321} 		{1}		{0}
+			{39322} 		{0}		{0}
+			{39323} 		{0}		{0}
+			{39324} 		{0}		{0}
+			{39325} 		{0}		{0}
+			{39326} 		{0}		{0}
+			{39327} 		{0}		{0}
+			{39328} 		{0}		{1}
+			{39329} 		{0}		{0}
+			{39347} 		{0}		{0}
+			{39348} 		{0}		{0}
+			{39349} 		{1}		{0}
+			{39350} 		{0}		{0}
+			{39351} 		{0}		{1}
+			{39352} 		{0}		{0}
+			{39353} 		{0}		{0}
+			{39354} 		{0}		{0}
+			{39355} 		{0}		{FAILED}
+			{39356} 		{0}		{FAILED}
+			{39357} 		{0}		{FAILED}
+			{39358} 		{0}		{FAILED}
+			{39359} 		{0}		{FAILED}
+			{39360} 		{0}		{FAILED}
+			{39361} 		{0}		{FAILED}
+			{39362} 		{0}		{FAILED}
+			{39363} 		{0}		{FAILED}
+			{39364} 		{0}		{FAILED}
+
 	 */
 	@Test
 	public void testEachMethod() throws Exception
